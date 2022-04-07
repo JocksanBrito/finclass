176b2a4ef8857392886725e2eb4accd820d3c4b7
San <san@Sans-MacBook-Pro.local>
2022-04-07 17:42:13 -0300

    Create BackButton component

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import Button from './components/Button';
 import Separator from './components/Separator';
 import Text from "./components/Text"
 import { Container } from './styles/style';
+import BackButton from './components/BackButton/index';
 
 const App : React.FC =  () => {
  const {typography} = useTheme();
   return<Container>
+    <BackButton />
     <Text>Finclass Ativa</Text>
     <Separator  height={20}/>
     <Button textType='bold' title="Assine agora"></Button>
