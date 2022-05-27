c34121cd765d12e56532beb933bbd087478903ff
san <jocksan.brittos@gmail.com>
2022-05-26 23:24:42 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Title>Finclass.v2</Title>
+    </Container>
+  );
+};
+
+export default App;
