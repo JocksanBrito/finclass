8ca3b0bec10ad2f28cbd5f11aab26685d2408f24
san <jocksan.brittos@gmail.com>
2022-05-29 20:08:11 -0300

    Add font

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const appColors = {
+  brand01: '#00E7F9',
+  neutralLightest: '#000000',
+  neutralMedium: '#8a8a8a',
+  neutralDark: '#202227',
+  neutralDarkest: '#000000',
+  neutralError: '#FF0000',
+};
+
+export default appColors;
